f50021b28b3dacaf9cb6357eccd1b7b126ea772b
Nguyễn Phúc Thịnh <thinh1769@gmail.com>
2023-03-21 19:15:44 +0700

    Thêm project cell, chưa kéo thả vào file swift

---
+++ HuTi/Cell/ProjectFilterResultTableViewCell/ProjectFilterResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProjectFilterResultTableViewCell.swift
+//  HuTi
+//
+//  Created by Nguyễn Thịnh on 21/03/2023.
+//
+
+import UIKit
+
+class ProjectFilterResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
